81ef410e975b31a4ef176c1df46e183b1cbdc671
PeterChang <peterchang2606696@gmail.com>
2024-01-09 17:44:57 +0800

    v0.0.5_error
    factor2 and za have errors

--- IdealizeSpetral.jl/exp/HSt42/.ipynb_checkpoints/For_test_Run_HS-checkpoint.jl
+++ IdealizeSpetral.jl/exp/HSt42/.ipynb_checkpoints/For_test_Run_HS-checkpoint.jl
@@ -3,7 +3,7 @@ using JGCM
 include("HS.jl")
 
 
-end_day = 5
+end_day = 50
 spinup_day = 0
 
 
@@ -16,7 +16,11 @@ op_man = Atmos_Spectral_Dynamics_Main(physics_params, end_day, spinup_day)
 # Finalize_Output!(op_man, "200day_convection_all.dat", "200day_convection_final.dat")
 # Finalize_Output!(op_man, "15day_test_all.dat", "15day_test_final.dat")
 # Finalize_Output!(op_man, "10day_test_all.dat", "10day_test_final.dat")
-Finalize_Output!(op_man, "test_all.dat", "test_final.dat")
+# Finalize_Output!(op_man, "test_all.dat", "test_final.dat")
+Finalize_Output!(op_man, "50day_test_all.dat", "50day_test_final.dat")
+# Finalize_Output!(op_man, "5day_test_all.dat", "5day_test_final.dat")
+
+
 
 # Finalize_Output!(op_man, "200day_edit_factor12_all.dat", "200day_edit_factor12_final.dat")
 # Finalize_Output!(op_man, "200day_edit_factor12_delta_t_all.dat", "200day_edit_factor12_delta_t_final.dat")
