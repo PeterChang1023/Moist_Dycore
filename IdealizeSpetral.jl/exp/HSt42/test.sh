267c398d683c84f29d00cdd50f455ecca50b4502
PeterChang <peterchang2606696@gmail.com>
2024-02-02 21:17:06 +0800

    v0.0.5add
    - Dycore.py is more clear edition of read_variable function

--- IdealizeSpetral.jl/exp/HSt42/test.sh
+++ IdealizeSpetral.jl/exp/HSt42/test.sh
@@ -2,12 +2,12 @@
 
 
 start_day=0
-final_day=20
-space_day=2
+final_day=1600
+space_day=100
 
 for i in `seq $start_day $space_day $final_day`
 do
-L=20
+L=10
 echo $i"day"
 echo -n $L > Latent_heat.txt
 
@@ -18,11 +18,11 @@ if [ $i -eq 0 ]; then
 	echo -n $space_day > HSt42_${L}/day_interval.txt
 	echo -n "None" > HSt42_${L}/firstday_file.txt
 else
-	echo -n "warmstart.dat" > HSt42_${L}/firstday_file.txt
+	echo -n "warmstart_${L}.dat" > HSt42_${L}/firstday_file.txt
 fi
 
 julia Run_HS.jl
-L=20 # To make sure that it wouldn't run the wrong L !!!
+L=10 # To make sure that it wouldn't run the wrong L !!!
      # When there are many L run simultaneously, L might be mistaken.
      # For example, 
      # If you run the first file L = 0, it takes you about 1 hour to run julia Run_HS.jl, 
@@ -31,13 +31,13 @@ L=20 # To make sure that it wouldn't run the wrong L !!!
      # it run the below code, it would take the output file to HSt42_10 (should've taken to HSt42_0) !!!
      # So to fix this, it should command second time to tell it the correct L !!!
 
-if [ -f "HSt42_${L}/warmstart.dat" ] && [ $i -lt $final_day ]; then
-	cp "HSt42_"${L}"/warmstart.dat" "warmstart.dat"
+if [ -f "HSt42_${L}/warmstart_${L}.dat" ] && [ $i -lt $final_day ]; then
+	cp "HSt42_"${L}"/warmstart_${L}.dat" "warmstart_${L}.dat"
 	mv "HSt42_"${L}"/all_L"${L}".dat" "HSt42_"${L}"/RH80_PR"$L"_"$final_day"day_startfrom_"$i"day_final.dat"
 	echo 'warmstart file exists.'
-elif [ -f "HSt42_${L}/warmstart.dat" ] && [ $i -eq $final_day ]; then
+elif [ -f "HSt42_${L}/warmstart_${L}.dat" ] && [ $i -eq $final_day ]; then
 	mv "HSt42_"${L}"/all_L"${L}".dat" "HSt42_"${L}"/RH80_PR"$L"_"$final_day"day_startfrom_"$i"day_final.dat"
-	mv "HSt42_"${L}"/warmstart.dat" "HSt42_"${L}"/HSt42_"${L}"RH80_PR"$L"_"$final_day"day_startfrom_"$i"day_final.dat"
+	mv "HSt42_"${L}"/warmstart_${L}.dat" "HSt42_"${L}"/HSt42_"${L}"RH80_PR"$L"_"$final_day"day_startfrom_"$i"day_final.dat"
 	echo "All files have completed!!!"
 fi
 done
