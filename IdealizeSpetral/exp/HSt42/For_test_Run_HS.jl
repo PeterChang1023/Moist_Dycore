32cedd451ac66b1a1a00f4f96864130b379ff19b
PeterChang1023 <nnnmmm5678@gmail.com>
2025-12-08 12:25:31 +0800

    fix all and final day files mismatched

--- IdealizeSpetral/exp/HSt42/For_test_Run_HS.jl
+++ IdealizeSpetral/exp/HSt42/For_test_Run_HS.jl
@@ -13,6 +13,6 @@ initial_day = 0 # warm start day
 physics_params = Dict{String,Float64}("σ_b"=>0.7, "k_f" => 1.0, "k_a" => 1.0/40.0, "k_s" => 1.0/4.0, "ΔT_y" => 60.0, "Δθ_z" => 10.0) 
 op_man = Atmos_Spectral_Dynamics_Main(physics_params, end_day, spinup_day, L)
 
-Finalize_Output!(op_man, "Dycore_50day_final_day.dat", "Dycore_50day_all_day.dat")
+Finalize_Output!(op_man, "Dycore_50day_all_day.dat", "Dycore_50day_final_day.dat")
 
 
